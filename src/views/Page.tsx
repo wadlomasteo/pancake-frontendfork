486198b20258e315c6258acd948635cba6709e0c
wadlomasteo <106463236+wadlomasteo@users.noreply.github.com>
2022-05-29 15:49:26 +0200

    Footer is back

--- src/views/Page.tsx
+++ src/views/Page.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import { Box, Flex } from '@pancakeswap/uikit'
+import Footer from 'components/Menu/Footer'
 import { PageMeta } from 'components/Layout/Page'
 import { EXCHANGE_DOCS_URLS } from 'config/constants'
 
@@ -32,6 +33,7 @@ const StyledPage = styled.div<{ $removePadding: boolean; $noMinHeight }>`
 const Page: React.FC<
   React.HTMLAttributes<HTMLDivElement> & {
     removePadding?: boolean
+    hideFooterOnDesktop?: boolean
     noMinHeight?: boolean
     helpUrl?: string
   }
