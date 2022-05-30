d8382fa8299647602b42619ba58cb329ae39c3bf
wadlomasteo <106463236+wadlomasteo@users.noreply.github.com>
2022-05-30 09:21:10 +0200

    Update Footer.tsx

--- packages/uikit/src/components/Footer/Footer.tsx
+++ packages/uikit/src/components/Footer/Footer.tsx
@@ -75,16 +75,7 @@ const MenuItem: React.FC<FooterProps> = ({
           flexDirection={["column", null, "row"]}
           justifyContent="space-between"
         >
-          <Flex order={[2, null, 1]} alignItems="center">
-            <ThemeSwitcher isDark={isDark} toggleTheme={toggleTheme} />
-            <LangSelector
-              currentLang={currentLang}
-              langs={langs}
-              setLang={setLang}
-              color={darkColors.textSubtle as keyof Colors}
-              dropdownPosition="top-right"
-            />
-          </Flex>
+          <Flex order={[2, null, 1]} alignItems="center"/>
           <Flex order={[1, null, 2]} mb={["0px", null, "0"]} justifyContent="space-between" alignItems="center"/>
         </StyledToolsContainer>
       </Flex>
